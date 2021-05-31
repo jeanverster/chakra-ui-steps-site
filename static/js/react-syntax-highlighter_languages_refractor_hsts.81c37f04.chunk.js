1b794578c9fbf42a04d64cdb5122945c48f9b71e
Jean Verster <jean@sovtech.com>
2021-05-31 19:02:32 +0200

    Updates

---
+++ static/js/react-syntax-highlighter_languages_refractor_hsts.81c37f04.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpchakra-ui-steps-site"]=this["webpackJsonpchakra-ui-steps-site"]||[]).push([[55],{273:function(s,a,e){"use strict";function t(s){s.languages.hsts={directive:{pattern:/\b(?:max-age=|includeSubDomains|preload)/,alias:"keyword"},safe:{pattern:/\d{8,}/,alias:"selector"},unsafe:{pattern:/\d{1,7}/,alias:"function"}}}s.exports=t,t.displayName="hsts",t.aliases=[]}}]);
+//# sourceMappingURL=react-syntax-highlighter_languages_refractor_hsts.81c37f04.chunk.js.map
---
+++ static/js/react-syntax-highlighter_languages_refractor_properties.ddaf5252.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpchakra-ui-steps-site"]=this["webpackJsonpchakra-ui-steps-site"]||[]).push([[106],{318:function(s,t,e){"use strict";function a(s){s.languages.properties={comment:/^[ \t]*[#!].*$/m,"attr-value":{pattern:/(^[ \t]*(?:\\(?:\r\n|[\s\S])|[^\\\s:=])+?(?: *[=:] *| ))(?:\\(?:\r\n|[\s\S])|[^\\\r\n])+/m,lookbehind:!0},"attr-name":/^[ \t]*(?:\\(?:\r\n|[\s\S])|[^\\\s:=])+?(?= *[=:] *| )/m,punctuation:/[=:]/}}s.exports=a,a.displayName="properties",a.aliases=[]}}]);
+//# sourceMappingURL=react-syntax-highlighter_languages_refractor_properties.ddaf5252.chunk.js.map
